9dd2bdc32334bbd1efeb1b2192b5895f7ad322e5
Luke Fromhold <luke@fromhold.io>
2022-08-08 18:59:40 +1000

    Set Requirements calls to use new vendor name

--- src/Fields/GroupedColorPaletteField.php
+++ src/Fields/GroupedColorPaletteField.php
@@ -22,7 +22,7 @@ class GroupedColorPaletteField extends GroupedDropdownField
      */
     public function Field($properties = [])
     {
-        Requirements::css('heyday/silverstripe-colorpalette:css/ColorPaletteField.css');
+        Requirements::css('fromholdio/silverstripe-colorpalette:css/ColorPaletteField.css');
 
         $source = $this->getSource();
 
